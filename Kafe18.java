b6f10b3e0d75c27463567a6f3e53dc295a022fc2
Adidtiasmara <mfirmanadid@gmail.com>
2024-11-21 16:44:44 +0700

    Percobaan 1

--- Kafe18.java
+++ Kafe18.java
@@ -1,7 +1,12 @@
 
 public class Kafe18 {
 
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan  + "!" );
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -12,8 +17,8 @@ public class Kafe18 {
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang anda inginkan. ");
     }
-
-    public static void main(String[] args) {
-        Menu();
+        
+        public static void main(String[] args) {
+            Menu("Andi", true);
     }
 }
